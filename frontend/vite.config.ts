a22e58e8651b47e978ddd3d5988332c03fab87f8
Oleksandr Igumnov <igumssukr@gmail.com>
2024-11-17 23:03:10 +0200

    Updated production config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default ({ mode }: any) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  process.env = { ...process.env, ...loadEnv(mode, `${process.cwd()}/env`) };
 
   // https://vitejs.dev/config/
   return defineConfig({
