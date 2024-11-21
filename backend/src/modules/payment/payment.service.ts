cd42192b58ffbf34dfb2150e9f1f23d5ce544701
Oleksandr Igumnov <igumssukr@gmail.com>
2024-11-21 09:07:20 +0200

    Fixed payment service

--- backend/src/modules/payment/payment.service.ts
+++ backend/src/modules/payment/payment.service.ts
@@ -33,16 +33,8 @@ export class PaymentService {
 
   public async charge(payload: ChargeDto): Promise<PaymentChargeResponse> {
     try {
-      console.log({
-        amount: payload.amount,
-        currency: process.env.STRIPE_CURRENCY || 'USD',
-        customer: payload.customerId,
-        payment_method: payload.paymentMethodId,
-        automatic_payment_methods: { enabled: false },
-        payment_method_types: ['card'],
-      })
       const paymentIntent = await this.stripeService.stripe.paymentIntents.create({
-        amount: payload.amount,
+        amount: payload.amount * 100,
         currency: process.env.STRIPE_CURRENCY || 'USD',
         customer: payload.customerId,
         payment_method: payload.paymentMethodId,
